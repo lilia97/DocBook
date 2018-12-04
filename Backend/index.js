9bacd74a7008bb310b4c2d36f3e07e6ed3ffc767
lilia97 <34647698+lilia97@users.noreply.github.com>
2018-12-04 16:00:00 +0100

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const api = require('./routes/api');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
+//const Aroutes = require('./routes/Aroutes.js');
+const Hroutes = require('./routes/Hroutes.js');
+const Troutes = require('./routes/Troutes.js');
+const cors = require('cors')
 const app = express();
-
- //CORS
-var cors = require('cors')
 app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-res.json({msg: 'This is CORS-enabled for all origins!'})
-})
  
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
- 
-
-
-app.use(bodyParser.json());
-// initilize routes
-app.use('/api', api);
-// error handling middelware
-app.use(function(err,req,res,next){
- //consol.log (err); 
- res.status(422).send({error: err.message});
-});
-
-
 mongoose.Promise = global.Promise;
 
 
@@ -34,7 +15,18 @@ mongoose.connect('mongodb://localhost:27017/docbookdb')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+app.use(bodyParser.json());
+//app.use('/Aroutes',Aroutes);
+app.use('/Hroutes',Hroutes);
+app.use('/Troutes',Troutes);
 
-app.listen( 3000, ()=>{
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+});
+app.get('/Docbook', (req,res)=>{
+    res.send('This is docbook')
+})
+
+app.listen(3000, ()=>{
     console.log('App is listening on port 3000')
 })
