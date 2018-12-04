36eb5f9ce78f8de1e502925e79b65f0fa5fe1295
asmamotmem <asma.motmem@medtech.tn>
2018-12-04 16:49:28 +0100

    fix

--- Backend/index.js
+++ Backend/index.js
@@ -11,12 +11,6 @@ const app = express();
  //CORS
 var cors = require('cors')
 app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-res.json({msg: 'This is CORS-enabled for all origins!'})
-})
- 
-
-
 
 app.use(bodyParser.json());
 // initilize routes
@@ -25,11 +19,6 @@ app.use('/api', Aroutes);
 app.use('/api', Hroutes);
 app.use('/api', Troutes);
 
-// error handling middelware
-app.use(function(err,req,res,next){
- //consol.log (err); 
- res.status(422).send({error: err.message});
-});
 
 
 mongoose.Promise = global.Promise;
