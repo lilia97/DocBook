5ddc16a85f6da739904b72965b2451558c513814
lilia <lilia@github.com>
2018-12-04 16:43:23 +0100

    index.js fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const api = require('./routes/api');
+const Aroutes = require('./routes/Aroutes');
+const Hroutes = require('./routes/Hroutes');
+const Troutes = require('./routes/Troutes');
+
 const bodyParser=require('body-parser');
 const app = express();
 
@@ -12,15 +15,16 @@ app.get('/products/:id', function (req, res, next) {
 res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
- 
+
 
 
 app.use(bodyParser.json());
 // initilize routes
 app.use('/api', api);
+app.use('/api', Aroutes);
+app.use('/api', Hroutes);
+app.use('/api', Troutes);
+
 // error handling middelware
 app.use(function(err,req,res,next){
  //consol.log (err); 
@@ -28,16 +32,6 @@ app.use(function(err,req,res,next){
 });
 
 
-=======
-const bodyParser = require('body-parser');
-//const Aroutes = require('./routes/Aroutes.js');
-const Hroutes = require('./routes/Hroutes.js');
-const Troutes = require('./routes/Troutes.js');
-const cors = require('cors')
-const app = express();
-app.use(cors())
- 
->>>>>>> 43f6655cc838561cce21aa769e9264f10f01e9ec
 mongoose.Promise = global.Promise;
 
 
@@ -45,23 +39,7 @@ mongoose.connect('mongodb://localhost:27017/docbookdb')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-<<<<<<< HEAD
 
 app.listen( 3000, ()=>{
-=======
-app.use(bodyParser.json());
-//app.use('/Aroutes',Aroutes);
-app.use('/api',Hroutes);
-app.use('/api',Troutes);
-
-app.get('/', (req,res)=>{
-    res.send('Hello World')
-});
-app.get('/Docbook', (req,res)=>{
-    res.send('This is docbook')
-})
-
-app.listen(3000, ()=>{
->>>>>>> 43f6655cc838561cce21aa769e9264f10f01e9ec
     console.log('App is listening on port 3000')
 })
